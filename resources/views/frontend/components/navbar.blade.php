0de375ccba25f417ed4f32a45b20c52b35337ee8
sheikhkamran1 <96003380+sheikhkamran1@users.noreply.github.com>
2022-11-17 23:23:41 +0545

    first module

---
+++ resources/views/frontend/components/navbar.blade.php
@@ -0,0 +1,25 @@
+<section class="header">
+    <nav>
+        <a href="index.html"> <img src="https://previews.123rf.com/images/captainvector/captainvector1703/captainvector170309945/74377645-university-logo-element.jpg" alt=""></a>
+        <div class="nav-links" id="navLinks">
+            <i class="fa fa-times" onclick="hideMenu()"></i>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">ABOUT</a></li>
+                <li><a href="">COURSES</a></li>
+                <li><a href="">GALLARY</a></li>
+                <li><a href="">BLOG</a></li>
+                <li><a href="">NEWS & EVENTS</a></li>
+                <li><a href="">CONTACT US</a></li>
+            </ul>
+           
+        </div>
+
+        <i class="fa fa-bars" onclick="showMenu()"></i>
+    </nav>
+    <div class="text-box">
+        <h1>Tulasi Mehar School & Collage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quisquam repudiandae veniam? <br> Eveniet adipisci architecto nobis, dolorum est doloribus assumenda quo, expedita atque, repellendus voluptate </p>
+        <a href="" class="hero-btn">APPLY NOW</a>
+    </div>
+</section>
